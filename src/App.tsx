fd27612c85b9d99a8da41f0a5bd5e9a91b34dc87
Masashi Kondo <masashi.kondo@rokka.dev>
2024-08-27 07:48:10 -0700

    fix style

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ export default function App() {
 					<Form.Guard allowValue='admin' name="type" >
 						Use this when you want to branch depending on the input value.
 					</Form.Guard>
-					<Form.Field.Select label="Language" name="language" options={LANGUAGE_TYPE_OPTIONS} />
+					<Form.Field.Select label="Language" name="language" options={LANGUAGE_TYPE_OPTIONS} required/>
 					<Form.ArrayField.Text label="Tags" name="tags" defaultValue='' />
 				</Form.Body>
 				<Form.Footer>
